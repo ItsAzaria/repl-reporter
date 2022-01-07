6f28bc8112dfab5b5cfbb47a697330a9ef0cf9bb
Azaria <moe9999@protonmail.com>
2022-01-07 19:33:05 +0000

    Flow to fetch required data and copy reponse to clipboard

--- repl-reporter.js
+++ repl-reporter.js
@@ -1,5 +1,39 @@
 const createReport = () => {
-    console.log('creating report');
+    const pathname = window.location.pathname.substring(1).split('/');
+    const username = pathname[0];
+    const repl = pathname[1];
+
+    if (document.body.innerHTML.includes('Forked from')) {
+        alert('This is a forked repl, you should report the root repl.');
+    }
+    // Forgive me father for I have sinned
+    const forks = document.querySelector('html > body > div > div > div> div > div > div > div > div > button > span').innerHTML;
+
+    if (!username || !repl) {
+        alert('Invalid repl link. Ensure the page you are currently on matches /@username/replname');
+        return;
+    }
+
+    if (!forks) {
+        alert('Document format changed. Contact moe#9999 on Discord.');
+        return;
+    }
+
+    fetch(`https://replit.com/data/repls/${username}/${repl}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const id = data.id;
+
+            const report = `[https://replit.com/${username}/${repl}], ${id}, other_repls, reason, ${forks}`;
+            
+            navigator.clipboard.writeText(report).then(() => {
+                alert('Report copied to your clipboard.');
+            }).catch((err) => {
+                console.log(err);
+            })
+            
+        });
+
 }
 
 document.onkeydown = (e) => {
